f46a26b830f58d4da281e63765e7ab7ba81e9a2b
bhdrYildiz <bahadiryildiz137@gmail.com>
2025-04-21 02:00:03 +0300

    animation added

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
             {/* Text Content*/}
             <div className='absolute z-[100] w-full h-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
                 <div className='flex flex-col items-center justify-center w-full h-full'>
-                    <div>
+                    <div data-aos="fade-up">
                         <h1 className='text-[25px] mb-4 md:mb-0 text-center md:text-[35px] lg:text-[45px] tracking-[0.7rem] text-white
                         font-bold uppercase'>
                             Lets Enjoy The Nature
@@ -37,7 +37,7 @@ const Hero = () => {
                         <span className='relative font-bold'>Search</span>
                     </Link>
                 </div>
-            </div> 
+            </div>
         </div>
     );
 };
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
 import Hero from './Hero/Hero'
 import Destination from './Destination/Destination'
 import Hotel from './Hotel/Hotel'
@@ -6,8 +8,26 @@ import WhyChoose from './WhyChoose/WhyChoose'
 import Review from './Review/Review'
 import News from './News/News'
 import NewsLetter from './NewsLetter/NewsLetter'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+
+    useEffect(() => {
+        const initAOS = async () => {
+            await import("aos");
+
+            AOS.init({
+                duration: 1000,
+                easing: "ease",
+                once: true,
+                anchorPlacement: "top-bottom",
+            });
+        };
+        initAOS();
+
+    }, []);
+
     return (
         <div className="overflow-hidden">
             <Hero />
